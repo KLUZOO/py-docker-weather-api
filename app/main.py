3b782ea9bd14a75da71027b3e500602eb4e352cd
Sasha <kluzodota@gmail.com>
2025-07-24 13:27:12 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ def get_weather() -> None:
         weather = requests.get(url)
         if weather.status_code == 200:
             json_file = weather.json()
-            print(f"{json_file["location"]["name"]}/"
-                  f"{json_file["location"]["country"]} "
-                  f"{json_file["location"]["localtime"]} "
-                  f"Weather: {json_file["current"]["temp_c"]} Celsius, "
-                  f"{json_file["current"]["condition"]["text"]}")
+            print(f"{json_file['location']['name']}/"
+                  f"{json_file['location']['country']} "
+                  f"{json_file['location']['localtime']} "
+                  f"Weather: {json_file['current']['temp_c']} Celsius, "
+                  f"{json_file['current']['condition']['text']}")
         else:
             print(f"Error: {weather.status_code}")
 
